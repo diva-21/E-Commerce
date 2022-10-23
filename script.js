e27ed4a968e25d0623bf5e79352df15e9c2478d9
Divakar <divakar2163@gmail.com>
2022-10-24 02:48:39 +0530

    add-cart started

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ if('close'){
         nav.classList.remove('active');
     })
 }
+const carts=document.querySelectorAll('.add-cart');
+for(let i=0;i<carts.length;i++){
+    carts[i].addEventListener('click',()=>{
+        cartNumbers();
+    })
+}
+function cartNumbers(){
+    localStorage.setItem('cartNumbers',1);
+}
