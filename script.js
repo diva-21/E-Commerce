e47dbc965436541aeee7188d903f147016d3966a
Divakar <divakar2163@gmail.com>
2022-10-24 13:04:27 +0530

    cart score added

--- script.js
+++ script.js
@@ -14,12 +14,58 @@ if('close'){
     })
 }
 // carts and products
+document.querySelector('.add-cart').addEventListener('click', function(event) {
+    event.preventDefault();
+  });
+let products=[
+    {
+        name:'Cartton T-shirt rainbow',
+        tag:'tshirtrainbow',
+        price:15,
+        inCart:0
+    },
+    {
+        name:'Cartton T-shirt green',
+        tag:'tshirtgreen',
+        price:15,
+        inCart:0
+    },
+    {
+        name:'Cartton T-shirt red',
+        tag:'tshirtred',
+        price:15,
+        inCart:0
+    },{
+        name:'Cartton T-shirt pink',
+        tag:'tshirtpink',
+        price:15,
+        inCart:0
+    }
+]
 const carts=document.querySelectorAll('.add-cart');
 for(let i=0;i<carts.length;i++){
-    carts[i].addEventListener('click',()=>{
+    carts[i].addEventListener('click',(event)=>{
         cartNumbers();
+        event.preventDefault(); // on click it will not refresh the whole page
     })
 }
-function cartNumbers(){
-    localStorage.setItem('cartNumbers',1);
+// to keep he cart count as constant even on refresh
+function onLoadCartNumbers(){
+    let productNumbers=localStorage.getItem('cartNumbers');
+    if(productNumbers){
+        document.querySelector('.cart-num span').textContent=productNumbers;
+    }
 }
+function cartNumbers(){
+    let productNumbers=localStorage.getItem('cartNumbers');
+    productNumbers=parseInt(productNumbers);
+    if(productNumbers){
+        localStorage.setItem('cartNumbers',productNumbers+1);
+        document.querySelector('.cart-num span').textContent=productNumbers+1;
+    }
+    else {
+        localStorage.setItem('cartNumbers',1);
+        document.querySelector('.cart-num span').textContent=1;
+    }
+}
+onLoadCartNumbers();
